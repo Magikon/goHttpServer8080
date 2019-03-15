7bceef1513e623e51026f2eaad1ab78796d4e375
Magikon <admin@zzillasoft.com>
2019-03-15 15:33:06 +0400

    change output

--- main.go
+++ main.go
@@ -1,40 +1,36 @@
 package main
 
 import (
-	"net/http"
-	//"strings"
 	"net"
+	"net/http"
 	"os"
+	"strconv"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
+func sayIP(w http.ResponseWriter, r *http.Request) {
 	var allIPs string
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		os.Exit(1)
 	}
 	for _, i := range ifaces {
+
 		addrs, err := i.Addrs()
 		if err != nil {
 			os.Exit(1)
 		}
 		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
+			if IPNett, ok := addr.(*net.IPNet); ok && !IPNett.IP.IsLoopback() {
+				allIPs += strconv.Itoa(i.Index) + ": " + addr.String() + "\r\n"
 			}
-			// process IP address
-			allIPs += ip.String() + ", "
 		}
 	}
-	message := "Container ip is - " + allIPs
+	message := "Container ips \r\n" + allIPs
 	w.Write([]byte(message))
 }
+
 func main() {
-	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/", sayIP)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
